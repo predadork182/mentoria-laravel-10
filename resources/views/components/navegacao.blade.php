dacf49ca9f19c05e65da8edb324d41339ca2e2d5
marcos  tavares <marcos.vinicius@itarget.com.br>
2023-10-28 13:43:47 -0300

    Criacao do painel list produtos paginacao

--- resources/views/components/navegacao.blade.php
+++ resources/views/components/navegacao.blade.php
@@ -7,25 +7,25 @@
       <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+            <a class="nav-link d-flex align-items-center gap-2 active " aria-current="page" href="#">
               <svg class="bi"><use xlink:href="#house-fill"/></svg>
               Dashboard
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link d-flex align-items-center gap-2" href="#">
+            <a class="nav-link d-flex align-items-center gap-2 text-secondary" href="#">
               <svg class="bi"><use xlink:href="#file-earmark"/></svg>
               Vendas
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link d-flex align-items-center gap-2" href="{{route('produto.index')}}">
+            <a class="nav-link d-flex align-items-center gap-2 text-secondary" href="{{route('produto.index')}}">
               <svg class="bi"><use xlink:href="#cart"/></svg>
               Produtos
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link d-flex align-items-center gap-2" href="#">
+            <a class="nav-link d-flex align-items-center gap-2 text-secondary" href="#">
               <svg class="bi"><use xlink:href="#people"/></svg>
               Clientes
             </a>
