bab4acab863e8222a37807924a94dc7784d18ede
marcos  tavares <marcos.vinicius@itarget.com.br>
2023-11-03 08:34:49 -0300

    criação do form update

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FormRequestProduto;
+use App\Models\Componentes;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
@@ -40,6 +41,8 @@ class ProdutosController extends Controller
 
         if ($request->method() == "POST") {
             $data = $request->all();
+            $componentes = new Componentes();
+            $data['valor'] = $componentes->formatacaoMascaraDinheiroDecimal($data['valor']);
             Produto::create($data);
 
             return redirect()->route('produto.index');
--- resources/views/pages/produtos/create.blade.php
+++ resources/views/pages/produtos/create.blade.php
@@ -9,14 +9,14 @@
     @csrf
     <div class="mb-3">
       <label for="nome" class="form-label">Nome</label>
-      <input type="text" class="form-control @error('nome') is-invalid @enderror" id="nome" name="nome">
+      <input type="text" class="form-control @error('nome') is-invalid @enderror" id="nome" name="nome" value="{{old('nome')}}">
       @if ($errors->has('nome'))
          <div class="invalid-feedback">{{$errors->first('nome')}}</div>
       @endif
     </div>
     <div class="mb-3">
       <label for="valor" class="form-label">Valor</label>
-      <input id="mascara_valor" type="text" class="form-control @error('valor') is-invalid @enderror" id="valor" name="valor">
+      <input id="mascara_valor" type="text" class="form-control @error('valor') is-invalid @enderror" id="valor" name="valor" value="{{old('valor')}}">
       @if ($errors->has('nome'))
           <div class="invalid-feedback">{{$errors->first('valor ')}}</div>
       @endif
