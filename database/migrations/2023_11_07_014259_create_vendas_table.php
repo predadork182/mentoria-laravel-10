14339cd844251adc8ccd220a23213883d1818f22
marcos  tavares <marcos.vinicius@itarget.com.br>
2023-11-07 20:29:05 -0300

    Criação do relacionamento da tabela de vendas com produtos e clientes

--- database/migrations/2023_11_07_014259_create_vendas_table.php
+++ database/migrations/2023_11_07_014259_create_vendas_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('vendas', function (Blueprint $table) {
             $table->id();
+            $table->integer('numero_da_venda');
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->foreignId('cliente_id')->constrained('clientes');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('vendas');
