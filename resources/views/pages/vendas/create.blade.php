81f9106cc613d0423598c9445d1b1b88c8f5d70f
mikek182 <marcosvinicius@geekwork.com.br>
2023-11-09 08:45:54 -0300

    Ateração da view de cadastro de vendas

--- resources/views/pages/vendas/create.blade.php
+++ resources/views/pages/vendas/create.blade.php
@@ -3,50 +3,15 @@
 
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Cadastrar novo cliente</h1>
+    <h1 class="h2">Cadastrar nova venda</h1>
 </div>
-<form class="form" method="POST" action="{{route('cadastrar.cliente')}}">
+<form class="form" method="POST" action="{{route('cadastrar.venda')}}">
     @csrf
     <div class="mb-3">
-      <label for="nome" class="form-label">Nome</label>
-      <input id="nome" type="text" class="form-control @error('nome') is-invalid @enderror" id="nome" name="nome" value="{{old('nome')}}">
-      @if ($errors->has('nome'))
-         <div class="invalid-feedback">{{$errors->first('nome')}}</div>
-      @endif
-    </div>
-    <div class="mb-3">
-      <label for="email" class="form-label">Email</label>
-      <input id="mascara_email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{old('email')}}">
-      @if ($errors->has('email'))
-          <div class="invalid-feedback">{{$errors->first('email')}}</div>
-      @endif
-    </div>
-    <div class="mb-3">
-      <label for="cep" class="form-label">CEP</label>
-      <input id="cep" type="text" class="form-control @error('cep') is-invalid @enderror" name="cep" value="{{old('cep')}}">
-      @if ($errors->has('cep'))
-          <div class="invalid-feedback">{{$errors->first('cep')}}</div>
-      @endif
-    </div>
-    <div class="mb-3">
-      <label for="endereco" class="form-label">Endereço</label>
-      <input id="endereco" type="text" class="form-control @error('endereco') is-invalid @enderror" name="endereco" value="{{old('endereco')}}">
-      @if ($errors->has('endereco'))
-          <div class="invalid-feedback">{{$errors->first('endereco')}}</div>
-      @endif
-    </div>
-    <div class="mb-3">
-      <label for="logradouro" class="form-label">Logradouro</label>
-      <input id="logradouro" type="text" class="form-control @error('logradouro') is-invalid @enderror" name="logradouro" value="{{old('logradouro')}}">
-      @if ($errors->has('logradouro'))
-          <div class="invalid-feedback">{{$errors->first('logradouro')}}</div>
-      @endif
-    </div>
-    <div class="mb-3">
-      <label for="bairro" class="form-label">Bairro</label>
-      <input id="bairro" type="text" class="form-control @error('bairro') is-invalid @enderror" name="bairro" value="{{old('bairro')}}">
-      @if ($errors->has('bairro'))
-          <div class="invalid-feedback">{{$errors->first('bairro')}}</div>
+      <label for="numero_da_venda" class="form-label">Nome</label>
+      <input id="numero_da_venda" type="text" class="form-control @error('numero_da_venda') is-invalid @enderror" id="numero_da_venda" name="numero_da_venda" value="{{old('numero_da_venda')}}">
+      @if ($errors->has('numero_da_venda'))
+         <div class="invalid-feedback">{{$errors->first('numero_da_venda')}}</div>
       @endif
     </div>
     <button type="submit" class="btn btn-success">Cadastrar</button>
