bba484b53a2efefb5098e7d5d2c868b0980033c8
marcos  tavares <marcos.vinicius@itarget.com.br>
2023-11-23 19:31:28 -0300

    Dados fake inseridos na seeder usuários

--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -2,16 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsuariosSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Marcos',
+            'email' => 'marcosvinicius@geekwork.com.br',
+            'password' =>  Hash::make('123654'),
+        ]);
     }
 }
